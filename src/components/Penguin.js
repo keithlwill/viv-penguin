7ec2a0a6b92a4a0e7cc72d1ba06b74bd6b0a5523
keithlwill <keith.will247@gmail.com>
2021-12-14 20:02:20 -0600

    Added a penguin and he is centered

---
+++ src/components/Penguin.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PingStand from "../images/penguins/penguin-stand.png";
+
+console.log(PingStand);
+
+export default function Penguin(props) {
+    const {width, height} = props
+    return (
+        <img className = "Penguin" src = {PingStand} alt = "Penguin goes here!" width = {width} height = {height}/>
+    );
+}
