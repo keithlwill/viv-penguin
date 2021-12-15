51d6269ad3705ee13bb1a8957ce00d43766a46ff
keithlwill <keith.will247@gmail.com>
2021-12-14 20:39:59 -0600

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Penguin from './components/Penguin';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Penguin width = {140} height = {200}/>
     </div>
   );
