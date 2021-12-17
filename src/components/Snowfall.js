3e5f54be2645fc342f3ef7087f2832160fcef3c8
Keith <keith.will247@gmail.com>
2021-12-17 17:36:11 -0600

    mild snow change

--- src/components/Snowfall.js
+++ src/components/Snowfall.js
@@ -2,21 +2,20 @@ import React from "react";
 import snow from "../images/penguins/snowflake.png"
 
 class Snowfall extends React.Component {
-    getPositions = (num, width) => {
+    getPositions = (num) => {
         let arr = [];
         for(let i = 0; i < num; i++) {
-            arr.push([Math.random(), Math.random()*2+1 + 's', Math.random()*-2-1 + 's']);
+            arr.push([Math.random(), Math.random()*5+1 + 's', Math.random()*-2-1 + 's']);
         }
         console.log(arr);
         return arr;
     }
 
-    positions = this.getPositions(20,500);
-    width = 0;
+    positions = this.getPositions(20);
 
     render() {
         return(
-            <div style = {{height: '100vh', width: '100vw', background: this.props.color || '#80c8ff', position: 'fixed', zIndex: -1, overflow: 'hidden'}} id = 'canvas'>
+            <div style = {{height: '100vh', width: '100vw', background: '#80c8ff', position: 'fixed', zIndex: -1, overflow: 'hidden'}} id = 'canvas'>
                 {this.positions.map(block => {
                     return (
                         <div style = {{positon: 'absolute', width: 0, height: 0}}>
